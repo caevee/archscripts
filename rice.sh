a5be8e1cd8ea58cd9a4a2c91e30edab74ab6d75f
caevee <pister.alex.cv@gmail.com>
2019-07-23 22:58:59 +0200

    Fixes

--- rice.sh
+++ rice.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-sudo pacman -S archlinux-wallpaper
-
 git clone https://github.com/addy-dclxvi/gtk-theme-collections
 
 git clone https://github.com/addy-dclxvi/openbox-theme-collections
 
 git clone https://github.com/caevee/.dotfiles
 
-sudo mv ~/gtk-theme-collection/Lumiere /usr/share/themes/
+sudo mv ~/gtk-theme-collections/Lumiere /usr/share/themes/
 
-sudo mv ~/openbox-theme-collection/Clia /usr/share/themes/
+sudo mv ~/openbox-theme-collections/Clia /usr/share/themes/
 
 mkdir ~/Pictures
-mkdir ~/wal
+mkdir ~/Pictures/wal
 
 cp ~/.dotfiles/autumnanimegirl.jpg ~/Pictures/wal/autumnanimegirl.jpg
 
